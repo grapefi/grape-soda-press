0a2f36ff7cccad8ab89da03eb68384a397a654a0
Tazz <1frenchstory@gmail.com>
2022-09-14 13:45:37 -0700

    replace buy xgrape by mint xgrape

--- components/Buy.jsx
+++ components/Buy.jsx
@@ -76,7 +76,7 @@ export default function Buy() {
                       active ? "bg-primary text-white" : "text-gray-900"
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                   >
-                    Buy xGrape
+                    Mint xGrape
                   </a>
                 )}
               </Menu.Item>
