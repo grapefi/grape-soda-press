eb7c5bf8eb0a7c344c99ff6ee4a3e8b3d9c8b5eb
Tazz <104685150+ItsMeTazz@users.noreply.github.com>
2023-03-23 10:01:20 -0700

    update to use dashboard instead

--- pages/index.js
+++ pages/index.js
@@ -44,16 +44,28 @@ export default function Home() {
           <Notification />
           <div className="grid gap-5 mx-auto max-w-5xl px-10">
             <div style={{ zIndex: 1 }} className="grid gap-4">
-              <TopDescription />
+              <div style={{ margin: "10px" }}>
+                <div className="text-5xl font-superstar top-description-title">
+                  Grape Soda Press
+                </div>
+                <div className="text-white">
+                  For the best User Experience, please use the Presses on{" "}
+                  <a
+                    className="underline font-bold"
+                    href="https://grapefinance.app/dashboard#presses"
+                  >{`Grape's Dashboard`}</a>
+                </div>
+              </div>
+              {/* <TopDescription />
 
-              <ProgressTimer />
+              <ProgressTimer /> */}
             </div>
           </div>
           {/* <div className="grid justify-center mx-auto mb-10 max-w-5xl px-5">
             <ToggleView />
           </div> */}
 
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4 mx-auto mb-10 max-w-5xl px-5 mt-10">
+          {/* <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4 mx-auto mb-10 max-w-5xl px-5 mt-10">
             <div style={{ zIndex: 1 }} className="grid gap-4">
               <Pool />
               <TopDepositedPrize />
@@ -64,13 +76,13 @@ export default function Home() {
               <Rewards />
               <Assassinate />
             </div>
-          </div>
-          <div className="grid grid-cols-1 mx-auto mb-10 max-w-5xl px-5">
+          </div> */}
+          {/* <div className="grid grid-cols-1 mx-auto mb-10 max-w-5xl px-5">
             {" "}
             <div style={{ zIndex: 1 }} className="grid gap-1">
               <Referrals />
             </div>
-          </div>
+          </div> */}
           <Footer />
         </NotificationContext.Provider>
       </main>
